0544ab7de154ee6d0a85985f2cbb8edce3e12da1
zt <183162113@qq.com>
2020-08-09 21:11:48 +0800

    vue通讯录项目初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <HelloWorld></HelloWorld><!-- //使用组件 -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  components:{
+    HelloWorld  //映射组件
+  }
+}
+</script>
+
+<style>
+</style>
